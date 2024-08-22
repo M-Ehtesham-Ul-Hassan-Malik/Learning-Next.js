0bbc8c51e92616f28e95cfb93212b95185f925f4
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-22 20:46:32 +0500

    learn and implement how to create components in Next.js

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Card from "@/component/Card";
 
 export default function Home() {
   return (
-   
+    <>
     <h1>Hello World</h1>
-    
+    <Card/>
+    </>
   );
 }
---
+++ my-app/component/Card.tsx
@@ -0,0 +1,12 @@
+
+
+const Card = () => {
+    let a = "This my first element.";
+    return(
+        <div>
+            <h2>{a}</h2>
+        </div>
+    )
+}
+
+export default Card;
