12eb0649a8b13902db388c63af711ea9e3ec5109
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-24 20:51:08 +0500

    learn to make the routes and pages dynamic in next.js

--- my-app/app/products/[product]/page.tsx
+++ my-app/app/products/[product]/page.tsx
@@ -1,10 +1,11 @@
 import Nav from "@/component/Navbar";
 
-export default function Product(){
+export default function Product({params}:{params:{product:string}}){
     return (
        
        <>
         <h1>Product Details</h1>
+        <p>{params.product}</p>
         </>
     );
 
--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -7,9 +7,9 @@ export default function Products(){
             <h1>Products List</h1>
 
             <ul>
-                <li><Link href="/products/product">Product 1</Link></li>
-                <li><Link href="/products/product">Product 2</Link></li>
-                <li><Link href="/products/product">Product 3</Link></li>
+                <li><Link href="/products/product1">Product 1</Link></li>
+                <li><Link href="/products/product2">Product 2</Link></li>
+                <li><Link href="/products/product3">Product 3</Link></li>
             </ul>
 
         </>
